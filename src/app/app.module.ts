11169f939ba33739cedb1b4d34033fb7f3089814
asd0905 <asd0905@jiran.com>
2018-06-11 18:47:05 +0900

    set title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { BrowserModule, Title }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 
@@ -40,6 +40,9 @@ import { isPlatformBrowser } from '@angular/common';
     MessagesComponent,
     HeroSearchComponent
   ],
+    providers: [
+      Title
+    ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
